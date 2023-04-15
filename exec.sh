994df2cecffbadcbfa8e02fae4efc4194a9fb8a1
[Silverainox] <serainox@gmail.com>
2023-04-15 10:29:18 +0200

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -4,44 +4,44 @@
 ADDONS_FOLDER="./Addons"
 
 # Function to execute the selected .sh or .py file
-function execute_panel() {
-  selected_panel="$1"
-  panel_folder=$(dirname "$selected_panel")
-  panel_name=$(basename "$panel_folder")
-  panel_file=$(basename "$selected_panel")
-  cd "$panel_folder"
-  if [[ "$panel_file" == *.sh ]]; then
-    bash "$panel_file"
-  elif [[ "$panel_file" == *.py ]]; then
-    python3 "$panel_file"
+function execute_module() {
+  selected_module="$1"
+  module_folder=$(dirname "$selected_module")
+  module_name=$(basename "$module_folder")
+  module_file=$(basename "$selected_module")
+  cd "$module_folder"
+  if [[ "$module_file" == *.sh ]]; then
+    bash "$module_file"
+  elif [[ "$module_file" == *.py ]]; then
+    python3 "$module_file"
   else
     zenity --error --text="Invalid file type!"
     exit 1
   fi
 }
 
-# Generate panel list based on .sh and .py files in Addons folder
-panel_list=()
+# Generate module list based on .sh and .py files in Addons folder
+module_list=()
 IFS=$'\n'
 for file in $(find "$ADDONS_FOLDER" -type f \( -name "*.sh" -o -name "*.py" \)); do
-  panel_folder=$(dirname "$file")
-  panel_name=$(basename "$file")
-  panel_list+=("[$(basename "$panel_folder")] $panel_name")
+  module_folder=$(dirname "$file")
+  module_name=$(basename "$file")
+  module_list+=("[$(basename "$module_folder")] $module_name")
 done
 unset IFS
 
-# If no panels are found, display an error message
-if [ ${#panel_list[@]} -eq 0 ]; then
-  zenity --error --text="No panel files found in Addons folder!"
+# If no modules are found, display an error message
+if [ ${#module_list[@]} -eq 0 ]; then
+  zenity --error --text="No module files found in Addons folder!"
   exit 1
 fi
 
-# Use Zenity to create a list dialog with the panel list
-selected_panel=$(zenity --list --title="Panel List" --text="Select a panel:" --column="Panel" "${panel_list[@]}" --height 400)
+# Use Zenity to create a list dialog with the module list
+selected_module=$(zenity --list --title="Module List" --text="Select a module:" --column="Module" "${module_list[@]}" --height 400)
 
-# Check if a panel is selected and call the function to execute it
-if [ -n "$selected_panel" ]; then
-  panel_name=$(echo "$selected_panel" | sed -E 's/\[([^]]+)\].*/\1/')
-  panel_file=$(echo "$selected_panel" | sed -E 's/.*\] (.*)/\1/')
-  execute_panel "$ADDONS_FOLDER/$panel_name/$panel_file"
+# Check if a module is selected and call the function to execute it
+if [ -n "$selected_module" ]; then
+  module_name=$(echo "$selected_module" | sed -E 's/\[([^]]+)\].*/\1/')
+  module_file=$(echo "$selected_module" | sed -E 's/.*\] (.*)/\1/')
+  execute_module "$ADDONS_FOLDER/$module_name/$module_file"
 fi
